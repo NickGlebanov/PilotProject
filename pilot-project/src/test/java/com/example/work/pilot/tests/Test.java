38fd00ae3162075507d62f4e97bf0424ff50a9a3
ZmixZ <termixator@yandex.ru>
2020-08-21 21:07:26 +0300

    Session 8

--- pilot-project/src/test/java/com/example/work/pilot/tests/Test.java
+++ pilot-project/src/test/java/com/example/work/pilot/tests/Test.java
@@ -22,15 +22,13 @@ public class Test {
   @org.testng.annotations.Test
   public void testS() throws Exception {
     enterWebsite();
-    driver.findElement(By.id("header-search")).sendKeys("nintendo switch");
-    driver.findElement(By.xpath("//button[@class='_1XiEJDPVpk']")).click();
-    String oneTab = driver.getWindowHandle(); // сохраняем первую вкладку
+    search(By.id("header-search"), By.xpath("//button[@class='_1XiEJDPVpk']"), "nintendo switch");
     type(By.linkText("Игровая приставка Nintendo Switch"));
-    ArrayList<String> newTab = new ArrayList<String>(driver.getWindowHandles()); // список всех вкладок
-    driver.switchTo().window(newTab.get(1)); // берем вторую вкладку
+    String oneTab = driver.getWindowHandle(); // сохраняем первую вкладку
+    getWindow(1);
     //new Select(driver.findElement(By.id("dropdown-control-1597759688456"))).selectByVisibleText("нет");
     type(By.linkText("Характеристики"));
-    String colorSwitch = driver.findElement(By.xpath("//div[@class='_2PZFauqN3Y']")).getAttribute("title");
+    String colorSwitch = getColor(By.xpath("//div[@class='_2PZFauqN3Y']"), "title");
 
 
     List<WebElement> list = new ArrayList();
@@ -38,16 +36,17 @@ public class Test {
     for(WebElement e : listElements) {
       list.add(e);   // Достуг того, что выгружены все характеристики, теперь надо добавить в модульный объект нужные
     }
+
     NintendoSpecification nt = new NintendoSpecification().withType(list.get(1).getText()).withScreen(list.get(3).getText()).
             withController(list.get(16).getText()).withAccessories(list.get(17).getText()).
             withColor(colorSwitch).withMemory(list.get(8).getText()).withWeight(list.get(15).getText());
 
 
-    search();
-    ArrayList<String> newTab1 = new ArrayList<String>(driver.getWindowHandles());
-    driver.switchTo().window(newTab1.get(2));
+    search(By.id("header-search"),By.xpath("//button[@class='_1XiEJDPVpk']"), "nintendo switch");
+    type(By.linkText("Игровая приставка Nintendo Switch Lite"));
+    getWindow(2);
     type(By.linkText("Характеристики"));
-    String colorLite = driver.findElement(By.xpath("//div[@class='_2PZFauqN3Y']")).getAttribute("title"); // сделать потом Коллекцию цветов и выбирать один из них
+    String colorLite = getColor(By.xpath("//div[@class='_2PZFauqN3Y']"), "title"); // сделать потом Коллекцию цветов и выбирать один из них
 
     List<WebElement> list1 = new ArrayList();
     List<WebElement> listElements1 = driver.findElements(By.tagName("dd"));
@@ -65,14 +64,22 @@ public class Test {
     Assert.assertEquals(nt.getScreen(), nt1.getScreen());
   }
 
+  public String getColor(By locator, String text) {
+    return driver.findElement(locator).getAttribute(text);
+  }
+
+  public void getWindow(int i) {
+    ArrayList<String> newTab = new ArrayList<String>(driver.getWindowHandles()); // список всех вкладок
+    driver.switchTo().window(newTab.get(i)); // берем вторую вкладку
+  }
+
   public void type(By locator) {
     driver.findElement(locator).click();
   }
 
-  public void search() {
-    driver.findElement(By.id("header-search")).sendKeys("nintendo switch");
-    driver.findElement(By.xpath("//button[@class='_1XiEJDPVpk']")).click();
-    type(By.linkText("Игровая приставка Nintendo Switch Lite"));
+  public void search(By locator, By locator1, String text) {
+    driver.findElement(locator).sendKeys(text);
+    driver.findElement(locator1).click();
   }
 
   public void enterWebsite() {
